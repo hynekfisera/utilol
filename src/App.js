00c08d0b60b708e904fd8143ee97cb219e497c76
Hynek Fišera <53520774+hynekfisera@users.noreply.github.com>
2021-07-28 21:09:46 +0200

    Match data & display result

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 const fetch = require("node-fetch");
 
 function App() {
-  const key = "?api_key=RGAPI-c0b8fb05-ed80-41d0-ae11-fb8a9aa6fbdd";
+  const key = "api_key=RGAPI-c0b8fb05-ed80-41d0-ae11-fb8a9aa6fbdd";
 
   const servers = [
     {
@@ -66,19 +66,59 @@ function App() {
 
   const [inputValue, setInputValue] = useState("");
 
+  const [resultText, setResultText] = useState("");
+
+  const [progress, setProgress] = useState(0);
+
   function getServerInfo(name) {
     return servers.find((server) => server.name === name);
   }
 
   async function getSummonerInfo(name) {
-    const link = "https://" + getServerInfo(server).host + "/lol/summoner/v4/summoners/by-name/" + name + key;
+    const link = "https://" + getServerInfo(server).host + "/lol/summoner/v4/summoners/by-name/" + name + "?" + key;
     return fetch("https://hynekfisera-cors-anywhere.herokuapp.com/" + link).then((response) => {
       return response.json();
     });
   }
 
-  async function getMaxDeaths() {
-    console.log(await getSummonerInfo(inputValue));
+  async function getMatches(puuid) {
+    const link = "https://" + getServerInfo(server).routing + "/lol/match/v5/matches/by-puuid/" + puuid + "/ids?start=0&count=50&" + key;
+    return fetch("https://hynekfisera-cors-anywhere.herokuapp.com/" + link).then((response) => {
+      return response.json();
+    });
+  }
+
+  async function getSingleMatch(id) {
+    const link = "https://" + getServerInfo(server).routing + "/lol/match/v5/matches/" + id + "?" + key;
+    return fetch("https://hynekfisera-cors-anywhere.herokuapp.com/" + link).then((response) => {
+      return response.json();
+    });
+  }
+
+  function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  async function getMaxDeaths(name) {
+    const summoner = await getSummonerInfo(name);
+    const matches = await getMatches(summoner.puuid);
+    let maxDeaths = 0;
+
+    for (const match of matches) {
+      const info = await getSingleMatch(match);
+      const deaths = info.info.participants.find((player) => player.puuid === summoner.puuid).deaths;
+      if (deaths > maxDeaths) {
+        maxDeaths = deaths;
+      }
+      setProgress((progress) => progress + 1);
+      await sleep(500);
+    }
+    return maxDeaths;
+  }
+
+  async function onSubmit() {
+    const result = await getMaxDeaths(inputValue);
+    setResultText(`${inputValue} has reached a maximal value of ${result} deaths in the last 50 games!`);
   }
 
   return (
@@ -100,7 +140,11 @@ function App() {
         </select>
       </header>
       <input type="text" placeholder="Summoner name" value={inputValue} onInput={(e) => setInputValue(e.target.value)} />
-      <button onClick={getMaxDeaths}>Submit</button>
+      <button onClick={onSubmit}>Submit</button>
+      <progress value={progress} max="50">
+        {progress}
+      </progress>
+      <span>{resultText}</span>
     </div>
   );
 }
