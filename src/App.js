07b1c21c1c4672eb8f5f5383f17c13a457f4b9bf
Hynek Fišera <53520774+hynekfisera@users.noreply.github.com>
2021-07-28 16:05:37 +0200

    API request

--- src/App.js
+++ src/App.js
@@ -1,31 +1,105 @@
 import { useState } from "react";
+const fetch = require("node-fetch");
 
 function App() {
-  const [host, setHost] = useState("eun1.api.riotgames.com");
+  const key = "?api_key=RGAPI-c0b8fb05-ed80-41d0-ae11-fb8a9aa6fbdd";
+
+  const servers = [
+    {
+      name: "BR1",
+      host: "br1.api.riotgames.com",
+      routing: "americas.api.riotgames.com",
+    },
+    {
+      name: "EUN1",
+      host: "eun1.api.riotgames.com",
+      routing: "europe.api.riotgames.com",
+    },
+    {
+      name: "EUW1",
+      host: "euw1.api.riotgames.com",
+      routing: "europe.api.riotgames.com",
+    },
+    {
+      name: "JP1",
+      host: "jp1.api.riotgames.com",
+      routing: "asia.api.riotgames.com",
+    },
+    {
+      name: "KR",
+      host: "kr.api.riotgames.com",
+      routing: "asia.api.riotgames.com",
+    },
+    {
+      name: "LA1",
+      host: "la1.api.riotgames.com",
+      routing: "americas.api.riotgames.com",
+    },
+    {
+      name: "LA2",
+      host: "la2.api.riotgames.com",
+      routing: "americas.api.riotgames.com",
+    },
+    {
+      name: "NA1",
+      host: "na1.api.riotgames.com",
+      routing: "americas.api.riotgames.com",
+    },
+    {
+      name: "OC1",
+      host: "oc1.api.riotgames.com",
+      routing: "americas.api.riotgames.com",
+    },
+    {
+      name: "TR1",
+      host: "tr1.api.riotgames.com",
+      routing: "europe.api.riotgames.com",
+    },
+    {
+      name: "RU",
+      host: "ru.api.riotgames.com",
+      routing: "europe.api.riotgames.com",
+    },
+  ];
+
+  const [server, setServer] = useState("EUN1");
+
+  const [inputValue, setInputValue] = useState("");
+
+  function getServerInfo(name) {
+    return servers.find((server) => (server.name = name));
+  }
+
+  async function getSummonerInfo(name) {
+    const link = "https://" + getServerInfo(server).host + "/lol/summoner/v4/summoners/by-name/" + name + key;
+    const response = await fetch(link);
+    console.log(response.json());
+  }
+
+  async function getMaxDeaths() {
+    console.log(getSummonerInfo(inputValue));
+  }
 
   return (
     <div className="app">
       <header>
         <select
-          value={host}
+          value={server}
           onChange={(e) => {
-            setHost(e.target.value);
+            setServer(e.target.value);
           }}
         >
-          <option value="br1.api.riotgames.com">BR1</option>
-          <option value="eun1.api.riotgames.com">EUN1</option>
-          <option value="euw1.api.riotgames.com">EUW1</option>
-          <option value="jp1.api.riotgames.com">JP1</option>
-          <option value="kr.api.riotgames.com">KR</option>
-          <option value="la1.api.riotgames.com">LA1</option>
-          <option value="la2.api.riotgames.com">LA2</option>
-          <option value="na1.api.riotgames.com">NA1</option>
-          <option value="oc1.api.riotgames.com">OC1</option>
-          <option value="tr1.api.riotgames.com">TR1</option>
-          <option value="	ru.api.riotgames.com">RU</option>
+          {servers.map((server) => {
+            return (
+              <option value={server.name} key={server.name}>
+                {server.name}
+              </option>
+            );
+          })}
         </select>
       </header>
-      {host}
+      <input type="text" placeholder="Summoner name" value={inputValue} onInput={(e) => setInputValue(e.target.value)} />
+      <button onClick={getMaxDeaths}>Submit</button>
     </div>
   );
 }
